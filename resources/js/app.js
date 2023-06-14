67eb187977bd12294438c65a71014abdd7056170
xinge <caoqixin@gmail.com>
2023-06-14 18:39:49 +0200

    inertia 配置, tailwindcss 继承

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,23 @@
-import './bootstrap';
+import { createApp, h } from "vue";
+import { Head, Link, createInertiaApp } from "@inertiajs/vue3";
+import "../css/app.css";
+import Layout from "./Shared/Layout/DefaultLayout.vue";
+
+createInertiaApp({
+    id: "app",
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        let page = pages[`./Pages/${name}.vue`];
+        page.default.layout = page.default.layout ?? Layout;
+
+        return page;
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .component("Link", Link)
+            .component("Head", Head)
+            .mount(el);
+    },
+    title: (title) => `My App - ${title}`,
+});
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/js/app.js"],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    // Vue 插件会重新编写资产 URL，以便在单文件组件中引用时，指向 Laravel web 服务器。
+                    // 将其设置为 `null`，则 Laravel 插件会将资产 URL 重新编写为指向 Vite 服务器。
+                    base: null,
+
+                    // Vue 插件将解析绝对 URL 并将其视为磁盘上文件的绝对路径。
+                    // 将其设置为 `false`，将保留绝对 URL 不变，以便可以像预期那样引用公共目录中的资源。
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
